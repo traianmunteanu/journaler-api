da0b5bcad4e946d38b09a624c6c8186328a43df6
Traian Munteanu <hello@traianmunteanu.com>
2020-01-16 20:23:36 +0200

    Journaler API: First Commit

---
+++ src/main/kotlin/com/journaler/JournalerApplication.kt
@@ -0,0 +1,11 @@
+package com.journaler
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JournalerApplication
+
+fun main(args: Array<String>) {
+    runApplication<JournalerApplication>(*args)
+}
